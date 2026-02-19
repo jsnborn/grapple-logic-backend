ff533c56fe161314355d4f1b6cea8b5bca0b82f3
jwong109 <jwong109@wgu.edu>
2026-02-19 13:34:26 -0800

    chore: initial commit with spring boot skeleton

---
+++ src/test/java/com/grapplelogic/engine/GrappleLogicEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grapplelogic.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrappleLogicEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
